c0cc53d5ed653940f9e96258d1a427c9cf7c4d24
David Burela <David.Burela@gmail.com>
2013-03-02 13:42:13 +1100

    Added in the SAPI helper library

---
+++ SensisShowdown/Helpers/SsapiSearcher.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace SensisShowdown.Helpers
+{
+    class SsapiSearcher
+    {
+        readonly Uri _endPoint;
+        readonly string _apiKey;
+
+        public SsapiSearcher(string endPoint, string apiKey)
+        {
+            _endPoint = new Uri(endPoint);
+            _apiKey = apiKey;
+        }
+
+        public async Task<SearchResponse> SearchFor(string query, string location)
+        {
+            // Build the API request
+            var url = new Uri(_endPoint, "?query=" + Uri.EscapeDataString(query) + "&location=" + Uri.EscapeDataString(location) + "&key=" + Uri.EscapeDataString(_apiKey));
+            var req = WebRequest.Create(url);
+
+            // Send the request and read the response
+            using (var res = await req.GetResponseAsync())
+            {
+                var serializer = new DataContractJsonSerializer(typeof(SearchResponse));
+                var resultStream = res.GetResponseStream();
+                var result = serializer.ReadObject(resultStream);
+                return result as SearchResponse;
+            }
+        }
+    }
+
+
+    public class SearchResponse
+    {
+        public int code;
+        public string message;
+        public int totalResults;
+        public Listing[] results;
+    }
+
+
+    public class Listing
+    {
+        public string name;
+        public Address primaryAddress;
+    }
+
+
+    public class Address
+    {
+        public string addressLine;
+        public string suburb;
+        public string state;
+        public string postcode;
+        public double latitude;
+        public double longitude;
+    }
+}
--- SensisShowdown/ViewModels/MainViewModel.cs
+++ SensisShowdown/ViewModels/MainViewModel.cs
@@ -1,10 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using SensisShowdown.Helpers;
 
 namespace SensisShowdown.ViewModels
 {
-    class MainViewModel
+    public class MainViewModel
     {
+        public MainViewModel()
+        {
+            FakeSearch();
+        }
+
+        public async void FakeSearch()
+        {
+            var searchEndPoint = "http://api.sensis.com.au/ob-20110511/test/search";
+            var apiKey = "jeyrqd8syqag4xp9v7m46g47";
+
+            var searcher = new SsapiSearcher(searchEndPoint, apiKey);
+
+            // Perform a search and check the response
+            var searchResponse = await searcher.SearchFor("asian", "melbourne");
+            if (searchResponse.code < 200 || searchResponse.code > 299)
+            {
+                //Console.WriteLine("Search failed - Error " + searchResponse.code + ": " + searchResponse.message);
+                return;
+            }
+
+            var total = searchResponse.totalResults;
+            
+            // Display the results
+            foreach (var result in searchResponse.results)
+            {
+                
+            }
+        }
+
+
+
+
+
+        //public void GetSearchResults(string term1, string term2)
+        //{
+            
+        //}
     }
 }
